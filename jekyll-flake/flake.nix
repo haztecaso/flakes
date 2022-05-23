f274cb001b61ce10e7eb8d9cabbaac0f70d28d2d
Adrián Lattes <adrianlattes@disroot.org>
2022-05-24 00:37:15 +0200

    module idea

--- jekyll-flake/flake.nix
+++ jekyll-flake/flake.nix
@@ -8,11 +8,25 @@
 
   outputs = { self, nixpkgs, utils, ... }@inputs:
   let
-      lib = import ./lib.nix;
+    lib = import ./lib.nix;
+    mkJekyll = lib.mkJekyll;
   in
     {
       overlay = final: prev: {
-        mkJekyll = lib.mkJekyll; 
+        mkJekyll = config: mkJekyll { inherit config; pkgs = final; }; 
+        # jekyllFull = mkJekyll {
+        #     pkgs = final;
+        #     config = {
+        #       gems = [
+        #         "jekyll-contentblocks"
+        #         "jekyll-image-size"
+        #         "jekyll-minifier"
+        #         "jekyll-seo-tag"
+        #         "jekyll-sitemap"
+        #         "ruby-thumbor"
+        #       ];
+        #     };
+        # };
         jekyllFull = final.callPackage ({ bundlerEnv, ruby }: bundlerEnv {
           name = "jekyllFull";
           inherit ruby;
@@ -23,13 +37,24 @@
       };
     } // utils.lib.eachDefaultSystem (system: let
         pkgs = import nixpkgs { inherit system; overlays = [ self.overlay ]; };
+        mkAppScript = name: script: {
+          type = "app";
+          program = "${pkgs.writeShellScriptBin name script}/bin/${name}";
+        };
       in rec {
         packages = {
+          # mkJekyll = pkgs.mkJekyll;
           jekyllFull = pkgs.jekyllFull;
-          mkJekyll = pkgs.mkJekyll;
         };
 
         defaultPackage = packages.jekyllFull;
+
+        apps = {
+          lock = mkAppScript "lock" ''
+              ${pkgs.bundler}/bin/bundle lock
+              ${pkgs.bundix}/bin/bundix
+          '';
+        };
 
         devShell = pkgs.mkShell {
           nativeBuildInputs = with pkgs; [ packages.jekyllFull bundler ruby nodejs ];
--- jekyll-flake/module.nix
+++ jekyll-flake/module.nix
@@ -1,18 +1,26 @@
-{ config, lib, pkgs }:
+{ config, lib, pkgs, ... }:
 {
   options = with lib; {
-    ruby-packages = mkOption {
-      type = types.lines;
-      default = ''
-      '';
-      example = ''
-        jekyll-contentblocks
-        jekyll-image-size
-        jekyll-minifier
-        jekyll-seo-tag
-        jekyll-sitemap
-        ruby-thumbor
-      '';
+    name = mkOption {
+      type = types.str;
+      default = "jekyll";
+      description = "Package name.";
     };
+    gems = mkOption {
+      type = with types; listOf str;
+      default = [];
+      example = [
+        "jekyll-contentblocks"
+        "jekyll-image-size"
+        "jekyll-minifier"
+        "jekyll-seo-tag"
+        "jekyll-sitemap"
+        "ruby-thumbor"
+      ];
+      description = "Ruby dependencies.";
+    };
+  };
+  config = {
+    gems = [ "jekyll" ];
   };
 }
